762df9e82c8b48358f8c6e544793d73300147048
malikrasyid <malikalrasyidi001@gmail.com>
2025-12-01 18:37:02 +0700

    change the api base url, ready to deploy in vercel

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import { useAuthStore } from "./store";
 
-const API_BASE_URL = 'http://localhost:5027/api';
+const API_BASE_URL = 'import.meta.env.VITE_PUBLIC_API_BASE_URL';
 
 // Helper for fetch requests
 async function request(url: string, options?: RequestInit) {
